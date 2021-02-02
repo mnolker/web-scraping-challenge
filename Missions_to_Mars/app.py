aa695df41c75764e0e02d42d5cf7b94cfde1ad3d
Melanie Nolker <melanie.nolker@kiewit.com>
2021-02-01 18:43:48 -0600

    scrape_mars.py updated

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,30 +1,41 @@
 # import necessary libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # create instance of Flask app
 app = Flask(__name__)
 
-
+# Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
 
 @app.route('/')
 def index():
-    mars_data = mongo.db.mars.find_one()
-    return render_template('index.html', marsdata=mars_data)
+    marsdata = mongo.db.marsdata.find()
+    return render_template('index.html', marsdata=marsdata)
 
 
-@app.route('/scrape')
+@app.route("/scrape")
 def scrape():
-    mars_data = mongo.db.marsdata
-    data = scrape_mars.scrape()
-    marsdata.update(
-        {},
-        data,
-        upsert=True
-    )
-    return redirect("/", code=302)
+    
+    # Run the scrape function
+    marsdata = scrape_mars.scrape()
 
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, marsdata, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+# alternative scrape
+    # marsdata = mongo.db.marsdata
+    # mars_data = scrape_mars.scrape()
+    # marsdata.update(
+    #     {},
+    #     mars_data,
+    #     upsert=True
+    # )
+    # return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -2,6 +2,8 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -21,28 +23,56 @@ def scrape():
 
     # create a soup object from the html
     soup = BeautifulSoup(html, 'html.parser')
-    results = soup.find('li', class_='slide')
-
-    top_news = results.find('div', class_='content_title').text
-    top_news_p = results.find('div', class_='article_teaser_body').text
+    results = soup.find('ul', class_='item_list')
+    results_title = results.find('li', class_='slide')
+    top_news = results_title.find('div', class_='bottom_gradient')
+    top_news_title = top_news.find('h3').text
+    top_news_p = results.find('div',class_='rollover_description_inner').text
 
     # add news title & p to mars_data dictionary
-    mars_data["news_title"] = top_news
+    mars_data["news_title"] = top_news_title
     mars_data["news_paragraph"] = top_news_p
+    print(top_news_title)
 
     #visit url & gather facts on Mars from table
     url_facts = 'https://space-facts.com/mars/'
     browser.visit(url_facts)
     soup = BeautifulSoup(html, 'html.parser')
     
-    tables = pd.read_html(url)
-    mars_data = tables[0]
-    df.columns = ['fact', 'Mars_value']
-    df = df.set_index('fact')
-    mars_table_html = df.to_html(classes='mars_facts_table')
+    tables = pd.read_html(url_facts)
+    mars_table = tables[0]
+    mars_table.columns = ['fact', 'Mars_value']
+    mars_table = mars_table.set_index('fact')
+    mars_table_html = mars_table.to_html(classes='mars_facts_table')
 
     # add it to our mars_data dict
-    mars_data["facts"] = build_report(mars_data)
+    mars_data["facts"] = mars_table_html
+
+    # hemispheres
+    url_hemipheres = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url_hemipheres)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    links = browser.find_by_css("a.itemLink.product-item h3")
+    print(len(links))
+
+    hemisphere_image_urls = []
+
+    for result in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css("a.itemLink.product-item h3")[result].click()
+        sample = browser.find_by_text("Sample").first
+        img_url = sample["href"]
+        img_title = browser.find_by_css("h2.title").text
+    
+        hemisphere['img_url'] = img_url
+        hemisphere['title'] = img_title
+        hemisphere_image_urls.append(hemisphere)
+    
+        browser.back()
+
+    mars_data["hemisphere_image_urls"] = hemisphere_image_urls
 
     browser.quit()
     return mars_data
